e5676214a8d2bb5587b107ece9fa35dcc50d097c
Nicholas Ericksen <nericksen.student@manhattan.edu>
2015-11-08 17:21:10 -0500

    Move trapz version of get_stokes to new file; change to experimental y instead of y_actual

---
+++ get_stokes.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+def get_stokes(theta, I):
+    signal0 = I
+    signal1 = signal0*np.sin(2*theta)
+    signal2 = signal0*np.cos(4*theta)
+    signal3 = signal0*np.sin(4*theta)
+
+    print "signal1 = ", signal1
+    print "signal2 = ", signal2
+    print "signal3 = ", signal3
+
+    denom = float(2*len(I))/360
+
+    area0 = 2 * np.trapz(signal0)/denom
+    area1 = 2 * np.trapz(signal1)/denom
+    area2 = 2 * np.trapz(signal2)/denom
+    area3 = 2 * np.trapz(signal3)/denom
+
+    A = area0/np.pi
+    B = -area1*2/np.pi
+    C = area2*2/np.pi
+    D = area3*2/np.pi
+
+    stokes0 = ((A-C))
+    stokes1 = (2*C)
+    stokes2 = (2*D)
+    stokes3 = (B)
+
+    stokes0p = np.sqrt(stokes1**2 + stokes2**2 + stokes3**2)
+
+    s0 = stokes0/stokes0p
+    s1 = stokes1/stokes0p
+    s2 = stokes2/stokes0p
+    s3 = stokes3/stokes0p
+
+    DOP = stokes0p/(stokes0)
+
+    return s0, s1, s2, s3
--- plot_txt_data.py
+++ plot_txt_data.py
@@ -2,6 +2,7 @@ from __future__ import division
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.pyplot as plt
+from get_stokes import get_stokes
 from scipy import integrate
 #here will be the loop to take data readings on each keystroke
 #readings will be then saved in a .txt file for processing
@@ -12,41 +13,51 @@ x = data[:,0]
 y = data[:,1]
 
 #first guess at curve before fitting
-offset = np.mean(data)
-amplitude = 3*np.std(data)/(np.sqrt(2))
-phase = 0
-freq = .02
-
-curve_guess = amplitude*np.sin(freq*x + phase) + offset
-
-z = np.polyfit(x, y, 6)
-f = np.poly1d(z)
-
-x_new = np.linspace(x[0], x[-1], 50)
-y_new = f(x_new)
+# offset = np.mean(data)
+# amplitude = 3*np.std(data)/(np.sqrt(2))
+# phase = 0
+# freq = .02
+#
+# curve_guess = amplitude*np.sin(freq*x + phase) + offset
+#
+# z = np.polyfit(x, y, 6)
+# f = np.poly1d(z)
+#
+# x_new = np.linspace(x[0], x[-1], 50)
+# y_new = f(x_new)
 
 x_radians = x * np.pi / 180
-print x_radians
+# print x_radians
 y_actual = 2 * np.sin( 4 * (x_radians - 0.4799655 )) + 3
-print y_actual
-integrandA = y_actual
-integrandB = y_actual*np.sin(2*x_radians)
-integrandC = y_actual*np.cos(4*x_radians)
-integrandD = y_actual*np.sin(4*x_radians)
+# print y_actual
+
+
+
+s0, s1, s2, s3 = get_stokes(x_radians, y)
+
+print "S0 (trapzoidal): ", s0
+print "S1 (trapzoidal): ", s1
+print "S2 (trapzoidal): ", s2
+print "S3 (trapzoidal): ", s3
+
+integrandA = y
+integrandB = y*np.sin(2*x_radians)
+integrandC = y*np.cos(4*x_radians)
+integrandD = y*np.sin(4*x_radians)
 
 A = 2 * (1/np.pi) * integrate.simps(integrandA, x_radians)
 B = 2 * (2/np.pi) * integrate.simps(integrandB, x_radians)
 C = 2 * (2/np.pi) * integrate.simps(integrandC, x_radians)
 D = 2 * (2/np.pi) * integrate.simps(integrandD, x_radians)
 
-print A
-print B
-print C
-print D
+# print A
+# print B
+# print C
+# print D
 
 S1 = 2 * C
 S2 = 2 * D
-S3 = B
+S3 = -B
 S0 = A - C
 # S0 = np.sqrt(S1**2 + S2**2 + S3**2)
 
@@ -55,18 +66,18 @@ norm_stokes_S1 = S1/S0
 norm_stokes_S2 = S2/S0
 norm_stokes_S3 = S3/S0
 
-unit_norm_denominator = np.sqrt(S1**2 + S2**2 + S3**2)
-unit_norm_S0 = S0 / unit_norm_denominator
-unit_norm_S1 = S1 / unit_norm_denominator
-unit_norm_S2 = S2 / unit_norm_denominator
-unit_norm_S3 = S3 / unit_norm_denominator
+# unit_norm_denominator = np.sqrt(S1**2 + S2**2 + S3**2)
+# unit_norm_S0 = S0 / unit_norm_denominator
+# unit_norm_S1 = S1 / unit_norm_denominator
+# unit_norm_S2 = S2 / unit_norm_denominator
+# unit_norm_S3 = S3 / unit_norm_denominator
 
-print "S0: " , unit_norm_S0
-print "S1: " , unit_norm_S1
-print "S2: " , unit_norm_S2
-print "S3: " , unit_norm_S3
+print "S0 (Simpsons): " , norm_stokes_S0
+print "S1 (Simpsons): " , norm_stokes_S1
+print "S2 (Simpsons): " , norm_stokes_S2
+print "S3 (Simpsons): " , norm_stokes_S3
 
-plt.plot(x, y, 'ro', x_new, y_new)
+plt.plot(x, y, 'ro')
 plt.plot(x, y_actual, 'bo')
 
 plt.title('Light Measuring Polarimetry')
@@ -86,7 +97,7 @@ x = np.cos(u)*np.sin(v)
 y = np.sin(u)*np.sin(v)
 z = np.cos(v)
 ax.plot_wireframe(x, y, z)
-ax.scatter(unit_norm_S1, unit_norm_S2, unit_norm_S3, color="g", s=100)
+ax.scatter(norm_stokes_S1, norm_stokes_S2, norm_stokes_S3, color="g", s=100)
 
 plt.xlabel('S1')
 plt.ylabel('S2')
