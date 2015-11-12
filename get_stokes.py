fbd2a1a599cad0e542b60a610582561521ec14c9
Nicholas Ericksen <nericksen.student@manhattan.edu>
2015-11-11 21:38:50 -0500

    Create seperate functions; save result img; refactor

--- get_stokes.py
+++ get_stokes.py
@@ -1,21 +1,22 @@
 import numpy as np
+from scipy import integrate
 
-def get_stokes(theta, I):
+def get_stokes_trapz(theta, I):
     signal0 = I
     signal1 = signal0*np.sin(2*theta)
     signal2 = signal0*np.cos(4*theta)
     signal3 = signal0*np.sin(4*theta)
 
-    print "signal1 = ", signal1
-    print "signal2 = ", signal2
-    print "signal3 = ", signal3
+    # print "signal1 = ", signal1
+    # print "signal2 = ", signal2
+    # print "signal3 = ", signal3
 
     denom = float(2*len(I))/360
 
-    area0 = np.trapz(signal0)/denom
-    area1 = np.trapz(signal1)/denom
-    area2 = np.trapz(signal2)/denom
-    area3 = np.trapz(signal3)/denom
+    area0 = np.trapz(signal0)
+    area1 = np.trapz(signal1)
+    area2 = np.trapz(signal2)
+    area3 = np.trapz(signal3)
 
     A = area0/np.pi
     B = -area1*2/np.pi
@@ -36,4 +37,48 @@ def get_stokes(theta, I):
 
     DOP = stokes0p/(stokes0)
 
+    print "S0 (trapzoidal): ", s0
+    print "S1 (trapzoidal): ", s1
+    print "S2 (trapzoidal): ", s2
+    print "S3 (trapzoidal): ", s3
+    print "DOP: ", DOP
+
     return s0, s1, s2, s3
+
+def get_stokes_simps(theta, I):
+    integrandA = I
+    integrandB = I*np.sin(2*theta)
+    integrandC = I*np.cos(4*theta)
+    integrandD = I*np.sin(4*theta)
+
+    A = (1/np.pi) * integrate.simps(integrandA, theta)
+    B = (2/np.pi) * integrate.simps(integrandB, theta)
+    C = (2/np.pi) * integrate.simps(integrandC, theta)
+    D = (2/np.pi) * integrate.simps(integrandD, theta)
+
+    stokes1 = 2 * C
+    stokes2 = 2 * D
+    stokes3 = -B
+    stokes0 = A - C
+    # S0 = np.sqrt(S1**2 + S2**2 + S3**2)
+
+    stokes0p = np.sqrt(stokes1**2 + stokes2**2 + stokes3**2)
+
+    S0 = stokes0/stokes0p
+    S1 = stokes1/stokes0p
+    S2 = stokes2/stokes0p
+    S3 = stokes3/stokes0p
+
+
+    DOP = np.sqrt(stokes1**2 + stokes2**2 + stokes3**2) / stokes0
+    # unit_norm_S0 = S0 / unit_norm_denominator
+    # unit_norm_S1 = S1 / unit_norm_denominator
+    # unit_norm_S2 = S2 / unit_norm_denominator
+    # unit_norm_S3 = S3 / unit_norm_denominator
+
+    print "S0 (Simpsons): " , S0
+    print "S1 (Simpsons): " , S1
+    print "S2 (Simpsons): " , S2
+    print "S3 (Simpsons): " , S3
+
+    return S0, S1, S2, S3
--- plot_txt_data.py
+++ plot_txt_data.py
@@ -2,7 +2,8 @@ from __future__ import division
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.pyplot as plt
-from get_stokes import get_stokes
+import numpy.fft as fft
+from get_stokes import get_stokes_trapz, get_stokes_simps
 from scipy import integrate
 #here will be the loop to take data readings on each keystroke
 #readings will be then saved in a .txt file for processing
@@ -12,94 +13,70 @@ data = np.loadtxt('data-set-1.txt')
 x = data[:,0]
 y = data[:,1]
 
-#first guess at curve before fitting
-# offset = np.mean(data)
-# amplitude = 3*np.std(data)/(np.sqrt(2))
-# phase = 0
-# freq = .02
-#
-# curve_guess = amplitude*np.sin(freq*x + phase) + offset
-#
-# z = np.polyfit(x, y, 6)
-# f = np.poly1d(z)
-#
-# x_new = np.linspace(x[0], x[-1], 50)
-# y_new = f(x_new)
-
 x_radians = x * np.pi / 180
 # print x_radians
 y_actual = 2 * np.sin( 4 * (x_radians - 0.4799655 )) + 3
 # print y_actual
 
+s0, s1, s2, s3 = get_stokes_trapz(x_radians, y)
+
+S0, S1, S2, S3 = get_stokes_simps(x_radians, y)
+
+def curve_fit(y):
+    # first guess at curve before fitting
+    offset = np.mean(y)
+    print "offset: ", offset
+    amplitude = 3*np.std(y)/(np.sqrt(2))
+    print "amplitude: ", amplitude
+
+    spectrum = fft.fft(data)
+    freq = fft.fftfreq(len(spectrum))
+    print "freq: ", freq
+    plt.plot(freq, abs(spectrum))
+    plt.show()
+    # phase = 0
+    # freq = .02 #this should be determined by the FFT spectrum
+    #
+    # curve_guess = amplitude*np.sin(freq*x + phase) + offset
+    #
+    # z = np.polyfit(x, y, 6)
+    # f = np.poly1d(z)
+    #
+    # x_new = np.linspace(x[0], x[-1], 50)
+    # y_new = f(x_new)
+curve_fit(y)
+
+def draw_data(x, y, y_actual):
+    plt.plot(x, y, 'ro')
+    plt.plot(x, y_actual, 'bo')
+
+    plt.title('Light Measuring Polarimetry')
+    plt.xlabel('Measurement Angles')
+    plt.ylabel('Intensity')
+
+    plt.show()
+
+draw_data(x, y, y_actual)
 
 
-s0, s1, s2, s3 = get_stokes(x_radians, y)
-
-print "S0 (trapzoidal): ", s0
-print "S1 (trapzoidal): ", s1
-print "S2 (trapzoidal): ", s2
-print "S3 (trapzoidal): ", s3
-
-integrandA = y
-integrandB = y*np.sin(2*x_radians)
-integrandC = y*np.cos(4*x_radians)
-integrandD = y*np.sin(4*x_radians)
-
-A = (1/np.pi) * integrate.simps(integrandA, x_radians)
-B = (2/np.pi) * integrate.simps(integrandB, x_radians)
-C = (2/np.pi) * integrate.simps(integrandC, x_radians)
-D = (2/np.pi) * integrate.simps(integrandD, x_radians)
-
-# print A
-# print B
-# print C
-# print D
-
-S1 = 2 * C
-S2 = 2 * D
-S3 = -B
-S0 = A - C
-# S0 = np.sqrt(S1**2 + S2**2 + S3**2)
-
-norm_stokes_S0 = S0/S0
-norm_stokes_S1 = S1/S0
-norm_stokes_S2 = S2/S0
-norm_stokes_S3 = S3/S0
-
-# unit_norm_denominator = np.sqrt(S1**2 + S2**2 + S3**2)
-# unit_norm_S0 = S0 / unit_norm_denominator
-# unit_norm_S1 = S1 / unit_norm_denominator
-# unit_norm_S2 = S2 / unit_norm_denominator
-# unit_norm_S3 = S3 / unit_norm_denominator
-
-print "S0 (Simpsons): " , norm_stokes_S0
-print "S1 (Simpsons): " , norm_stokes_S1
-print "S2 (Simpsons): " , norm_stokes_S2
-print "S3 (Simpsons): " , norm_stokes_S3
-
-plt.plot(x, y, 'ro')
-plt.plot(x, y_actual, 'bo')
-
-plt.title('Light Measuring Polarimetry')
-plt.xlabel('Measurement Angles')
-plt.ylabel('Intensity')
-
-plt.show()
-
-#initalize plotting
-fig = plt.figure()
-ax = fig.gca(projection='3d')
 
 
-#draw poincare sphere
-u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
-x = np.cos(u)*np.sin(v)
-y = np.sin(u)*np.sin(v)
-z = np.cos(v)
-ax.plot_wireframe(x, y, z)
-ax.scatter(norm_stokes_S1, norm_stokes_S2, norm_stokes_S3, color="g", s=100)
+def draw_poincare_sphere(S1, S2, S3):
+    #initalize plotting
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
 
-plt.xlabel('S1')
-plt.ylabel('S2')
+    u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+    x = np.cos(u)*np.sin(v)
+    y = np.sin(u)*np.sin(v)
+    z = np.cos(v)
+    ax.plot_wireframe(x, y, z)
+    ax.scatter(S1, S2, S3, color="g", s=100)
 
-plt.show()
+    plt.xlabel('S1')
+    plt.ylabel('S2')
+
+    plt.show()
+
+draw_poincare_sphere(S1, S2, S3)
+draw_poincare_sphere(s1, s2, s3)
