76ff15ff1546cfd21a6f86fb92b0fce6740695b5
Nicholas Ericksen <nericksen.student@manhattan.edu>
2015-12-06 16:07:07 -0500

    Print out each variable; comment non essential code

--- get_stokes.py
+++ get_stokes.py
@@ -47,22 +47,33 @@ def get_stokes_trapz(theta, I):
 
 def get_stokes_simps(theta, I):
     theta = np.array(theta)
+    print "theta = ", theta
     I = np.array(I)
+    print "I = ", I
+
+    n1 = np.sin(2*theta)
+    n2 = np.cos(4*theta)
+    n3 = np.sin(4*theta)
+
+    print "n1 ", n1
+    print "n2 ", n2
+    print "n3 ", n3
+
 
     integrandA = I
     integrandB = I*np.sin(2*theta)
     integrandC = I*np.cos(4*theta)
     integrandD = I*np.sin(4*theta)
 
-    # print integrandA
-    # print integrandB
-    # print integrandC
-    # print integrandD
+    print "integrandA ", integrandA
+    print "integrandB ", integrandB
+    print "integrandC ", integrandC
+    print "integrandD ", integrandD
 
-    A = (1/np.pi) * integrate.simps(integrandA, theta)
-    B = -(2/np.pi) * integrate.simps(integrandB, theta)
-    C = (2/np.pi) * integrate.simps(integrandC, theta)
-    D = (2/np.pi) * integrate.simps(integrandD, theta)
+    A = (2/np.pi) * integrate.simps(integrandA, theta)
+    B = -(4/np.pi) * integrate.simps(integrandB, theta)
+    C = (4/np.pi) * integrate.simps(integrandC, theta)
+    D = (4/np.pi) * integrate.simps(integrandD, theta)
 
     print "============================="
     print "A: " , A
@@ -83,18 +94,35 @@ def get_stokes_simps(theta, I):
     # S1 = stokes1/stokes0p
     # S2 = stokes2/stokes0p
     # S3 = stokes3/stokes0p
+    DOP = np.sqrt(stokes1**2 + stokes2**2 + stokes3**2) / (stokes0)
+    print "DOP", DOP
 
-    S0 = stokes0
-    S1 = stokes1
-    S2 = stokes2
-    S3 = stokes3
+    # S0 = stokes0 / DOP
+    # S1 = stokes1 / DOP
+    # S2 = stokes2 / DOP
+    # S3 = stokes3 / DOP
+    #
+    S0_un = stokes0
+    S1_un = stokes1
+    S2_un = stokes2
+    S3_un = stokes3
+    S0 = stokes0 / stokes0
+    S1 = stokes1 / stokes0
+    S2 = stokes2 / stokes0
+    S3 = stokes3 / stokes0
 
 
-    DOP = np.sqrt(stokes1**2 + stokes2**2 + stokes3**2) / stokes0
+
     # unit_norm_S0 = S0 / unit_norm_denominator
     # unit_norm_S1 = S1 / unit_norm_denominator
     # unit_norm_S2 = S2 / unit_norm_denominator
     # unit_norm_S3 = S3 / unit_norm_denominator
+
+
+    print "S0 (un): " , S0_un
+    print "S1 (un): " , S1_un
+    print "S2 (un): " , S2_un
+    print "S3 (un): " , S3_un
 
     print "S0 (Simpsons): " , S0
     print "S1 (Simpsons): " , S1
